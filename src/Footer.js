e1d4e7373e58a7ed7302960a5f1290dbf34008b6
papegassama <papesouleyegassma@gmail.com>
2026-05-01 13:39:56 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+function Footer(){
+    return(
+        <footer className="footer">
+            <p className="footer-texte">© 2026 SenTransport
+                 - ESP/UCAD. Tous droits réservés.</p>
+        </footer>
+    );
+}
+
+export default Footer;
